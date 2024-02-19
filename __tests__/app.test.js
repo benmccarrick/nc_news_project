c78fc1fc5ae14fdee7b2fbc4a2cf34fba83e20d2
Ben McCarrick <b.mccarrick4326@gmail.com>
2024-02-19 16:00:31 +0000

    task 4 get articles by id completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,13 +30,13 @@ describe("GET /api/topics", () => {
         });
       });
   });
-});
+})
 describe("GET /api/topicsss", () => {
     test("status:404, responds with appropriate error status when provided a route that does not exist", () => {
-  return request(app)
-   .get("/api/topicsss")
-   .expect(404)
-   })
+    return request(app)
+      .get("/api/topicsss")
+      .expect(404)
+   });
 })
 describe("GET /api", () => {
     test("status:200, responds with an object describing all the available endpoints on the API", () => {
@@ -45,7 +45,6 @@ describe("GET /api", () => {
         .expect(200)
         .then(({ body }) => {
           const { endpoints } = body;
-          console.log(endpoints)
           expect(endpoints).toBeInstanceOf(Object);
           expect(endpoints).hasOwnProperty('description')
           expect(endpoints).hasOwnProperty('queries')
@@ -53,4 +52,40 @@ describe("GET /api", () => {
           expect(endpoints).toEqual(endpointsFile)
         });
     });
+})
+describe("GET /api/articles/:article_id", () => {
+    test("status:200, responds with a single article", () => {
+      return request(app)
+        .get("/api/articles/2")
+        .expect(200)
+        .then(({body}) => {
+            const {article} = body;
+            console.log(article)
+            expect(article.article_id).toBe(2)
+            expect(article.title).toBe('Sony Vaio; or, The Laptop')
+            expect(article.topic).toBe('mitch')
+            expect(article.author).toBe('icellusedkars')
+            expect(article.body).toBe('Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.')
+            const date = new Date((1602828180000)-3600000)
+            expect(article.created_at).toBe(date.toISOString())
+            expect(article.votes).toBe(0)
+            expect(article.article_img_url).toBe('https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700')
+        });
+   });
+   test("status:404 sends an appropriate status and error message when given a valid but non-existent id", () => {
+    return request(app)
+      .get("/api/articles/222222")
+      .expect(404)
+      .then(({body}) => {
+        expect(body.msg).toBe('article does not exist');
+      });
+  });
+  test("status:400 sends an appropriate status and error message when given an invalid id", () => {
+    return request(app)
+      .get("/api/articles/not-an-article-id")
+      .expect(400)
+      .then(({body}) => {
+        expect(body.msg).toBe('Bad request');
+      });
+  });
 })
