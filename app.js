e0479dd5f9137a31c6266bef59646f61162f5684
Ben McCarrick <b.mccarrick4326@gmail.com>
2024-02-20 11:21:16 +0000

    updating previous tasks

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const {getTopics} = require('./controllers/topics-controller')
 const {getAvailableEndpoints} = require('./controllers/endpoints-controller')
 const {getArticleById, getArticles} = require('./controllers/articles-controller')
-const {handlePsqlErrors, handleCustomErrors, handleInternalErrors} = require('./controllers/errors-controller')
+const {handlePsqlErrors, handleCustomErrors, handleInternalErrors, handleInvalidEndpoints} = require('./controllers/errors-controller')
 
 const app = express();
 
@@ -12,6 +12,7 @@ app.get("/api", getAvailableEndpoints);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getArticles);
 
+app.all("/*", handleInvalidEndpoints)
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
 app.use(handleInternalErrors);
