d94a249b12e2d57be732578f9e136f5266a13c7a
Ben McCarrick <b.mccarrick4326@gmail.com>
2024-02-20 15:59:33 +0000

    Task 7 post new comment complete

--- controllers/errors-controller.js
+++ controllers/errors-controller.js
@@ -1,13 +1,17 @@
 exports.handleInvalidEndpoints = (req, res, next) => {
-    res.status(404).send({msg: "Path not found"})
+    res.status(404).send({msg: "Path not found"});
+    next(err);
 }
 
 exports.handlePsqlErrors = (err, req, res, next) => {
-    if (err.code === "22P02") {
-        res.status(400). send({ msg: "Bad request" });
-    } else {
-        next(err);
+    if (err.code === "22P02" || err.code === "23502") {
+        res.status(400).send({ msg: "Bad request" });
     }
+    if (err.code === "23503"){
+        res.status(404).send({ msg: "Not found" });
+    }
+    
+        next(err);
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
