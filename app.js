0c61c0b61c61b4feca9597755d53a4ca19767802
Ben McCarrick <b.mccarrick4326@gmail.com>
2024-02-20 12:10:34 +0000

    Task 6 get comments by article id complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const {getTopics} = require('./controllers/topics-controller')
 const {getAvailableEndpoints} = require('./controllers/endpoints-controller')
 const {getArticleById, getArticles} = require('./controllers/articles-controller')
+const {getCommentsByArticleId} = require('./controllers/comments-controller')
 const {handlePsqlErrors, handleCustomErrors, handleInternalErrors, handleInvalidEndpoints} = require('./controllers/errors-controller')
 
 const app = express();
@@ -11,6 +12,7 @@ app.get("/api/topics", getTopics);
 app.get("/api", getAvailableEndpoints);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 
 app.all("/*", handleInvalidEndpoints)
 app.use(handlePsqlErrors);
