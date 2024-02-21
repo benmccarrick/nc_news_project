406c0280c395fc5c4e6aafe5b011a6d873111a2c
Ben McCarrick <b.mccarrick4326@gmail.com>
2024-02-21 12:23:45 +0000

    Task 10 get users complete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const {getTopics} = require('./controllers/topics-controller')
 const {getAvailableEndpoints} = require('./controllers/endpoints-controller')
 const {getArticleById, getArticles, updateArticles} = require('./controllers/articles-controller')
 const {getCommentsByArticleId, addCommentsByArticleId, deleteCommentById} = require('./controllers/comments-controller')
+const {getUsers} = require('./controllers/users-controller')
 const {handlePsqlErrors, handleCustomErrors, handleInternalErrors, handleInvalidEndpoints} = require('./controllers/errors-controller')
 
 const app = express();
@@ -14,11 +15,15 @@ app.get("/api", getAvailableEndpoints);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+app.get("/api/users", getUsers);
+
 app.post("/api/articles/:article_id/comments", addCommentsByArticleId);
+
 app.patch("/api/articles/:article_id", updateArticles);
+
 app.delete("/api/comments/:comment_id", deleteCommentById);
 
-app.all("/*", handleInvalidEndpoints)
+app.all("/*", handleInvalidEndpoints);
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
 app.use(handleInternalErrors);
